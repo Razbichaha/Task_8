27d09746c09154b3847e1c70bec3f0f70d669ec9
Сергей <seregabur@live.ru>
2022-03-24 21:04:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество старушек ожидающих очередь");
+            int receptionTimeMin = 10;
+            string temp = Console.ReadLine();
+            int numberOfWaiting;
+            bool sheck = int.TryParse(temp, out numberOfWaiting);
+            if (sheck)
+            {
+                if (numberOfWaiting != 0)
+                {
+                    int hour = numberOfWaiting * receptionTimeMin / 60;
+                    int minute = numberOfWaiting * receptionTimeMin % 60;
+
+                    Console.WriteLine("Время ожидания - " + hour + " час " + minute + " минут");
+                }
+                else
+                {
+                    Console.WriteLine("Поздравляем очереди нет вы можете пройти");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не корректные данные \nв следующий раз будте внимательны\nвводите колличество ожидающих бабулек");
+            }
+
+        
+        }
+    }
+}
